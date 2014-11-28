6233f09af6f4b3eabbdd35ab38697aaf3f82aeb7
Edwin Vermeer <edwin@evict.nl>
2014-11-28 15:55:50 +0100

    should have been disabled

--- AppMessage/AppMessage/AppDelegate.swift
+++ AppMessage/AppMessage/AppDelegate.swift
@@ -14,8 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                             
     var window: UIWindow?
 
-
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: NSDictionary?) -> Bool {        
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: NSDictionary?) -> Bool {
+        
         // Make sure we receive subscription notifications
         application.registerUserNotificationSettings(UIUserNotificationSettings(forTypes: .Alert | .Badge | .Sound, categories: nil))
         application.registerForRemoteNotifications()
@@ -23,11 +23,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Only call this line once, ever. It will make sure the recordType are there in iCloud.
         // This call is here to help you play around with this code.
         // After this, go to the iCloud dashboard and make all metadata for each recordType queryable and sortable!
-//        EVCloudKitDao.instance.createRecordTypes([Message(), Asset(), News()])
+        EVCloudKitDao.instance.createRecordTypes([Message(), Asset(), News()])
         
         return true
     }
-
     
     func application(application: UIApplication!, didReceiveRemoteNotification userInfo: [NSObject : NSObject]!) {
         NSLog("Push received")
@@ -35,30 +34,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             NSLog("Not a CloudKit Query notification.")            
         })
     }
-    
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-    
     
 }
 
