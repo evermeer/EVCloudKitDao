df54adbb0245a637707a22117be389613d7caf95
Edwin Vermeer <edwin@evict.nl>
2014-12-24 15:56:51 +0100

    dummy for cocoapods

---
+++ AppMessage/AppMessage/CloudKit/Dummy.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+Dummy.h
+//  AppMessage
+//
+//  Created by Edwin Vermeer on 12/24/14.
+//  Copyright (c) 2014 mirabeau. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dummy : NSObject
+
+@end
---
+++ AppMessage/AppMessage/CloudKit/Dummy.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+Dummy.m
+//  AppMessage
+//
+//  Created by Edwin Vermeer on 12/24/14.
+//  Copyright (c) 2014 mirabeau. All rights reserved.
+//
+
+#import "Dummy.h"
+
+@implementation Dummy
+
+@end
