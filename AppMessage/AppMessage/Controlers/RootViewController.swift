3cccc4bee2bb325082208c84e82905eb27acb7f7
Vermeer, Edwin <Edwin.Vermeer@clockwork.nl>
2017-09-11 20:41:33 +0200

    Update for Swift 4

--- AppMessage/AppMessage/Controlers/RootViewController.swift
+++ AppMessage/AppMessage/Controlers/RootViewController.swift
@@ -36,10 +36,13 @@ class RootViewController: UIViewController {
     }
 
     /**
-    As what user are we loged in to iCloud. Then open the main app.
+     
+    As what ugetUserser are we loged in to iCloud. Then open the main app.
     */
     func getUser(_ retryCount:Double = 1) {
-        self.loginLabel.isHidden = true
+        Async.main {
+            self.loginLabel.isHidden = true
+        }
         
         EVCloudKitDao.publicDB.requestDiscoverabilityPermission({ (granted) -> Void in
             if !granted {
